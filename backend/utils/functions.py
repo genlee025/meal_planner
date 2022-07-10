042db1557029b42b55c73d7d9378913f29d6bf45
= <gen.lee025@gmail.com>
2022-07-10 21:11:03 +1000

    sunday night

---
+++ backend/utils/functions.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+def add_meal(name:str,ingredients:list,method:str):
+    return None
+
+def add_pantry_item(name:str,unit:str,quantity:int,expiry:datetime.date):
+    return None
+
+def generate_meal(usepantry:bool, num_meals:int):
+    return None
